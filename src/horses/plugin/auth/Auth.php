175b3cff91afc1f2c52b435ab873a43de4243513
antoine.pouch <antoine.pouch@ML-13809.nurun.com>
2013-03-28 12:55:17 -0400

    Static method should be static

--- src/horses/plugin/auth/Auth.php
+++ src/horses/plugin/auth/Auth.php
@@ -55,7 +55,7 @@ class Auth
      * @param string $password
      * @return string
      */
-    public function getPasswordHash($password)
+    public static function getPasswordHash($password)
     {
         return sha1($password);
     }
